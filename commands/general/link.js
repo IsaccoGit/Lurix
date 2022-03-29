a2ce84f97f46c789c58e911220e95f1981deb163
isacco8bot <isaccobertola8@gmail.com>
2022-03-29 09:45:45 +0200

    Update link.js

--- commands/general/link.js
+++ commands/general/link.js
@@ -1,4 +1,3 @@
-
 module.exports = {
     name: "link",
     data: {
@@ -8,8 +7,8 @@ module.exports = {
     async execute(interaction) {
         var embed = new Discord.MessageEmbed()
             .setTitle("__Link IMPORTANTI__")
-            .addField("Link bot", "Clicca [qui](https://discord.com/api/oauth2/authorize?client_id=952916222213771294&permissions=8&scope=bot%20applications.commands) per invitare il bot")
-            .addField("Link server", "Ecco il link del server: https://dsc.gg/lurix.support")
+            .addField("Link bot", "Clicca [qui](https://discord.com/api/oauth2/authorize?client_id=958268971478556743&permissions=8&scope=bot%20applications.commands) per invitare il bot")
+            .addField("Link server", "Ecco il link del server: https://discord.gg/dsFYz6mscX")
             .setColor(configColor.AZZURRO)
         interaction.reply({ embeds: [embed], ephemeral: true })
     }
