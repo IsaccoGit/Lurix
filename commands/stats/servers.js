da9e3012f435389c3f43d0d1bb4277e6f05a85cf
isacco8bot <isaccobertola8@gmail.com>
2022-04-15 09:37:42 +0200

    Fix bugs

--- commands/stats/servers.js
+++ commands/stats/servers.js
@@ -18,7 +18,7 @@ module.exports = {
             .setDescription("Tutti i server in cui è presente il bot")
             .addField("Servers 💻", client.guilds.cache.size.toString())
             .addField("Members 👥", client.users.cache.size.toString())
-            .setFooter({ text: server.name, iconURL: "https://cdn.discordapp.com/attachments/885923288554037290/962396566247850104/Server_logo.png" })
+            .setFooter({ text: server.name, iconURL: server.iconURL })
             .setTimestamp()
         interaction.reply({ embeds: [embed] })
     }
